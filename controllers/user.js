bf2a1f433cd560e9a3362e87d73d8fadc306e3da
KK_Mac_16 <kimangkheang@gmail.com>
2024-03-25 20:13:46 +0700

    Remove comment

--- controllers/user.js
+++ controllers/user.js
@@ -46,7 +46,6 @@ const googleLogin = (asyncHandler(async (req, res) => {
 
 const handleGoogleLogin = (asyncHandler(async (req, res) => {
     const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_REDIRECT}&response_type=code&scope=profile email`
-    // Show OAuth2.0 Consent Dialog
     res.redirect(url)
 }))
 
