bf8edc2f9fdecef18e91c4763464c73a85a351fe
KK_Mac_16 <kimangkheang@gmail.com>
2024-03-29 11:39:57 +0700

    Handle vue routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
 const compression = require('compression')
 const port = 3000
 const user = require("./routes/user.js")
@@ -10,7 +9,7 @@ const tweet = require("./routes/tweet.js")
 const morgan = require('morgan')
 const responseTime = require('response-time')
 const cors = require('cors')
-var session = require('express-session')
+const path = require("path");
 require('dotenv').config()
 
 
@@ -25,11 +24,21 @@ app.use(cors())
 app.use(responseTime())
 app.use(morgan('combined'))
 app.use(compression())
-app.use(jsonParser)
-app.use(express.static("frontend/dist"))
+app.use(bodyParser.json())
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
 app.use("/api/users", passport.authenticate('jwt', { session: false }), user)
 app.use("/api/tweets", passport.authenticate('jwt', { session: false }), tweet)
 app.use("/api/auth", auth)
+
+// Route all requests to index.html for Vue routing to handle
+// All routes for API need to put before catch all routes
+// So put `*` at the end
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
